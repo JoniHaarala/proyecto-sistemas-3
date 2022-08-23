1fb37fdf2359386c4f623f68fa3bf19fb3c6abf8
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-23 11:32:41 -0300

    added bgColor extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'main-bg': '#FAFBFB',
+        'main-dark-bg': '#20232A',
+        'secondary-dark-bg': '#33373E',
+        'light-gray': '#F7F7F7',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      }
+    },
   },
   plugins: [],
-  darkMode: 'class',
+  darkMode: 'class'
 }
