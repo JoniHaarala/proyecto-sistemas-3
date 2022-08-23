7189d1dd22da4018f03d9e61af46a0387407d310
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-22 21:46:22 -0300

    first sprint dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Routes, Route, Link } from "react-router-dom";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
+
+  const activeMenu = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex relative dark:bg-gray-900">
+      <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+        <TooltipComponent content="settings" position="Top">
+          <button
+            type='button'
+            className="text-white flex text-3xl p-3 hover:bg-slate-300 hover:drop-shadow-lg"
+            style={{ background: 'blue', borderRadius: '50%' }}>
+            <SettingsIcon />
+          </button>
+        </TooltipComponent>
+      </div>
+      {
+        activeMenu ? (
+          <div className="fixed w-72 sidebar dark:bg-gray-900 bg-gray-50">
+
+          </div>
+        ) : (
+          <div className='w-0 dark:bg-gray-900'>
+
+          </div>
+        )
+      }
+      <div className={`dark:bg-gray-50 bg-gray-50 min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`}>{/* estas comillas (``) se llaman template string y sirven para usar variables dentro de un string */}
+      
+      </div>
     </div>
   );
 }
