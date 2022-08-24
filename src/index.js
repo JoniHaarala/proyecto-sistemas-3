cf20d694099cbc74ba26b63e3b87ecacded2c920
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-24 03:19:06 -0300

    added ContextApi

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ContextProvider } from './context/ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>
 );
 
