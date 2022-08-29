9bdcd3afadbd014fda2270c5bab0aabe63d9bbed
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-29 00:23:53 -0300

    mod head styles

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = ({ category, title }) => (
   <div className=" mb-10">
-    <p className="text-lg text-gray-600">{category}</p>
-    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+    <p className="text-lg text-gray-600">
+      {category}
+    </p>
+    <p className="text-3xl font-extrabold tracking-tight text-gray-800 dark:text-gray-200">
       {title}
     </p>
   </div>
