59d4273980928e649751ff947e4508a922f01fa2
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-31 23:24:07 -0300

    formulario pagFact v1

--- src/pages/f-Facturacion/PayFact.jsx
+++ src/pages/f-Facturacion/PayFact.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Header from '../../components/Head';
+export default class PayFact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'coconut' };
 
-export default function PayFact() {
-  return (
-    <div>PayFact</div>
-  )
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-gray-700 dark:text-gray-50 rounded-3xl">
+        <Header category="Facturas" title="Pagar factura" />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Pick your favorite flavor:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
+
