3ee086bdf8349081e755814d4bccc6a4e5319e78
jonatan <jonatan.haaralaorosco@gmail.com>
2022-09-23 12:12:05 -0300

    Added Calendar app

--- src/pages/Apps/Calendar.jsx
+++ src/pages/Apps/Calendar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
+import React from 'react';
+import '../../App.css'
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import { scheduleData } from '../../data/configData';
+import moment from 'moment';
+export class App extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.data = scheduleData;
+    }
+    render() {
+        return <ScheduleComponent height='550px' selectedDate={new Date(moment().format('YYYY'),moment().format('MM'),moment().format('DD'))} eventSettings={{ dataSource: this.data,
+            fields: {
+                id: 'Id',
+                subject: { name: 'Subject' },
+                isAllDay: { name: 'IsAllDay' },
+                startTime: { name: 'StartTime' },
+                endTime: { name: 'EndTime' }
+            }
+        }}>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+    </ScheduleComponent>;
+    }
 }
-
-export default Calendar
