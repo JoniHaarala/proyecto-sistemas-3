8220db7e09af86231ef0d861cc95dea48713ea9b
jonatan <jonatan.haaralaorosco@gmail.com>
2022-11-08 23:33:06 -0300

    pie data added

--- src/data/configData.js
+++ src/data/configData.js
@@ -286,18 +286,18 @@ export const links = [
                 name: 'pie',
                 icon: <PieChartIcon />,
             },
-            // {
-            //     name: 'financial',
-            //     icon: <CandlestickChartIcon />,
-            // },
+            {
+                name: 'financial',
+                icon: <CandlestickChartIcon />,
+            },
             {
                 name: 'color-mapping',
                 icon: <SignalCellularAltIcon />,
             },
-            {
-                name: 'pyramid',
-                icon: <ChangeHistoryIcon />,
-            },
+            // {
+            //     name: 'pyramid',
+            //     icon: <ChangeHistoryIcon />,
+            // },
             {
                 name: 'stacked',
                 icon: < StackedBarChartIcon />,
@@ -997,13 +997,9 @@ export const lineCustomSeries = [
 ];
 
 export const pieChartData = [
-    { x: 'Labour', y: 18, text: '18%' },
-    { x: 'Legal', y: 8, text: '8%' },
-    { x: 'Production', y: 15, text: '15%' },
-    { x: 'License', y: 11, text: '11%' },
-    { x: 'Facilities', y: 18, text: '18%' },
-    { x: 'Taxes', y: 14, text: '14%' },
-    { x: 'Insurance', y: 16, text: '16%' },
+    { x: 'temporario', y: 42, text: '42.86%' },
+    { x: 'alquiler', y: 28, text: '28.57%' },
+    { x: 'ventas', y: 28, text: '28.47%' },
 ];
 
 export const contextMenuItems = [
