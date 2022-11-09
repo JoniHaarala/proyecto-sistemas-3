0f09c1c09dcb7f06caa9e4479a590f9a2d85b4f8
jonatan <jonatan.haaralaorosco@gmail.com>
2022-11-08 23:37:19 -0300

    pie chart done

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -17,7 +17,7 @@ const Doughnut = ({ id, data, legendVisiblity, height }) => {
       <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
       <AccumulationSeriesCollectionDirective>
         <AccumulationSeriesDirective
-          name="Sale"
+          name="operacion"
           dataSource={data}
           xName="x"
           yName="y"
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -5,7 +5,7 @@ import { ChartsHeader, PieChart } from '../../components';
 
 const Pie = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+    <ChartsHeader category="Pie" title="Propiedades por tipo de operacion" />
     <div className="w-full">
       <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
     </div>
